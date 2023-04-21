1906e1b6539b5473154fc05c7c7406c9dba733fe
Tudor Alexandru <tudorale2005@gmail.com>
2023-04-21 20:37:32 +0300

    removing framer motion

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,18 +10,8 @@ export default function App({Component, pageProps}) {
 
   return (
   <>
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={router.pathname}
-        initial={{ opacity: 0, }}
-        animate={{ opacity: 1, }}
-        exit={{ opacity: 0, }}
-        transition={{ duration: 0.35 }}
-        className="base"
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+
+    <Component {...pageProps} />
   </>
     
   )
