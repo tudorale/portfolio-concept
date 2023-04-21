f451add42af33603f6b1d0f1c5eb0bbf1d75a37e
Tudor Alexandru <tudorale2005@gmail.com>
2023-04-21 20:06:18 +0300

    test deploy

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,18 +10,7 @@ export default function App({Component, pageProps}) {
 
   return (
   <>
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={router.route}
-        initial={{ opacity: 0, }}
-        animate={{ opacity: 1, }}
-        exit={{ opacity: 0, }}
-        transition={{ duration: 0.35 }}
-        className="base"
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <Component {...pageProps} />
   </>
     
   )
